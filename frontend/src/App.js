f6891416112aa72de0db9a8f0c124808e48fc852
Ritik Khandelwal <ritikkh14012000@gmail.com>
2023-11-27 12:20:53 +0530

    fe-router,product,home screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+import HomeScreen from './screens/HomeScreen'
+import { Routes, Route } from "react-router-dom";
+import ProductScreen from './screens/ProductScreen'
 
 const App = () => {
-  return (
-    <div>Welcome to eComm</div>
+  return (<>
+  <Header/>
+  <main className='py-3'>
+    <Container>
+      <Routes>
+        <Route path='/' element={<HomeScreen/>}/>
+        <Route path='/product/:id' element={<ProductScreen/>}/>
+      </Routes>
+    </Container>
+  </main>
+  <Footer/>
+  </>
   )
 }
 
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import {Navbar,Nav,Container} from 'react-bootstrap';
+import {FaShoppingCart,FaUser} from 'react-icons/fa';
+import logo from '../assets/logo.png';
+import {LinkContainer} from "react-router-bootstrap"
+
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+      <Container>
+        <LinkContainer to="/">        
+          <Navbar.Brand href="#home"><img src={logo} alt='eComm' width="28px" height="28px"/> eComm</Navbar.Brand>
+        </LinkContainer>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <LinkContainer to="/cart">              
+              <Nav.Link><FaShoppingCart/> Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+              <Nav.Link><FaUser/> Sign In</Nav.Link>
+            </LinkContainer>            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </header>
+  )
+}
+
+export default Header
