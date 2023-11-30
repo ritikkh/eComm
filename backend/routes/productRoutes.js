87cea8ad57d1203695b3d255e7b52bfde6acedc1
Ritik Khandelwal <ritikkh14012000@gmail.com>
2023-11-30 14:16:59 +0530

    seeder,api created,middleware

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+import {getProducts, getProductById} from '../controllers/productController.js'; 
+// router.get("/",getProducts);
+// router.get("/:id",getProductById);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
+export default router;
